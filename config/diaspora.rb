0d14a73261e4bc91c89425f12f98b42367d7de1f
brunoguerra <ti@woese.com>
2013-06-19 19:26:14 -0300

    Complete running

---
+++ config/diaspora.rb
@@ -0,0 +1,3 @@
+
+ENV['DIASPORA_HOME'] ||= File.expand_path("../../../diaspora", __FILE__)
+ENV['DB'] ||= 'postgres'
--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,7 @@
 # Load the rails application
-require File.expand_path('../application', __FILE__)
 
 require 'rubygems'
-require 'diaspora_plugin'
+#require 'diaspora_plugin'
 
-ENV['DIASPORA_HOME'] ||= File.expand_path("../../../diaspora", __FILE__)
-ENV['DB']='postgres'
-
-Diaspora.diaspora_env do
-  require File.join(ENV['DIASPORA_HOME'], 'config', 'environment')
-end
+require File.expand_path('../application', __FILE__)
+Diaspora.initialize_environment!
